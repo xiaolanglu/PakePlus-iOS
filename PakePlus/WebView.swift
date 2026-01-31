6f627b20da09e0237acc5a1f684aa27259f76ec7
1024小神 <15670339118@163.com>
2026-01-31 17:06:53 +0800

    Update Info.plist to include UIBackgroundModes for audio and adjust web URL format; enhance WebView configuration for universal access and media playback capabilities.

--- PakePlus/WebView.swift
+++ PakePlus/WebView.swift
@@ -19,6 +19,9 @@ struct WebView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let webConfiguration = WKWebViewConfiguration()
         webConfiguration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        webConfiguration.setValue(true, forKey: "allowUniversalAccessFromFileURLs")
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.allowsPictureInPictureMediaPlayback = true
         // enable developer extras
         if #available(iOS 16.4, *) {
             webConfiguration.preferences.setValue(true, forKey: "developerExtrasEnabled")
